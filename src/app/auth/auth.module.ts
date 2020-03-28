a0f27d18113dc02b1e9f94b90429bdc2f410e45d
naphattharawat <naphattharawat@gmail.com>
2020-03-29 01:00:20 +0700

    env

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -12,8 +12,8 @@ export function tokenGetter() {
     JwtModule.forRoot({
       config: {
         tokenGetter,
-        whitelistedDomains: ['localhost:3000'],
-        blacklistedRoutes: ['localhost:3001/login/']
+        whitelistedDomains: ['localhost:3000', '192.168.4.177:3000', '192.168.4.149:3000'],
+        // blacklistedRoutes: ['localhost:3000/login/']
       }
     })
   ]
