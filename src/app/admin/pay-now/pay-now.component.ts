18f8911bcee9f1e488d33b1f4a13dd105d33883c
peemppk <peemppk5538@gmail.com>
2020-04-01 13:19:31 +0700

    push

--- src/app/admin/pay-now/pay-now.component.ts
+++ src/app/admin/pay-now/pay-now.component.ts
@@ -19,6 +19,7 @@ export class PayNowComponent implements OnInit {
   hospName: any;
 
   modal = false;
+  loading = false;
 
   @ViewChild('hospital') hospitals: AutocompleteHospitalComponent;
   constructor(
