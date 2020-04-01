4584442f149d34d62d501e331cb53736c91da60c
gun khemnak <gunnn_boy_hand@hotmail.com>
2020-04-01 11:35:24 +0700

    pull

--- src/app/admin/layout/layout.component.ts
+++ src/app/admin/layout/layout.component.ts
@@ -29,10 +29,10 @@ export class LayoutComponent implements OnInit {
     this.rights = decoded.rights;
     this.fullname = decoded.fullname;
     this.hospname = decoded.hospname;
-    this.restockMenu = findIndex(this.rights, 'ADMIN_RESTOCK') === -1 ? false : true;
-    this.minMaxMenu = findIndex(this.rights, 'ADMIN_MIN_MAX') === -1 ? false : true;
-    this.userManageMenu = findIndex(this.rights, 'ADMIN_USER_MANAGE') === -1 ? false : true;
-    this.suppliesManageMenu = findIndex(this.rights, 'ADMIN_SUPPLIES_MANAGE') === -1 ? false : true;
+    this.restockMenu = findIndex(this.rights, { name: 'ADMIN_RESTOCK' }) === -1 ? false : true;
+    this.minMaxMenu = findIndex(this.rights, { name: 'ADMIN_MIN_MAX' }) === -1 ? false : true;
+    this.userManageMenu = findIndex(this.rights, { name: 'ADMIN_USER_MANAGE' }) === -1 ? false : true;
+    this.suppliesManageMenu = findIndex(this.rights, { name: 'ADMIN_SUPPLIES_MANAGE' }) === -1 ? false : true;
   }
 
   ngOnInit() {
--- src/app/user/layout/layout.component.ts
+++ src/app/user/layout/layout.component.ts
@@ -15,13 +15,13 @@ export class LayoutComponent implements OnInit {
   collapsible = true;
   collapse = false;
   rights: any;
-  requisitionMenu: any;
-  checkBedMenu: any;
-  checkSuppliesMenu: any;
-  balanceSuppliesMenu: any;
-  balanceBedMeny: any;
-  statusTrackingMenu: any;
-  settingMenu: any;
+  requisitionMenu;
+  checkBedMenu;
+  checkSuppliesMenu;
+  balanceSuppliesMenu;
+  balanceBedMeny;
+  statusTrackingMenu;
+  settingMenu;
 
   public jwtHelper = new JwtHelperService();
   constructor(
@@ -30,13 +30,16 @@ export class LayoutComponent implements OnInit {
     const decoded = this.jwtHelper.decodeToken(sessionStorage.getItem('token'));
     this.fullname = decoded.fullname;
     this.hospname = decoded.hospname;
-    this.requisitionMenu = findIndex(this.rights, 'STAFF_REQUISITION') === -1 ? false : true;
-    this.checkBedMenu = findIndex(this.rights, 'STAFF_CHECK_BED') === -1 ? false : true;
-    this.checkSuppliesMenu = findIndex(this.rights, 'STAFF_CHECK_SUPPLIES') === -1 ? false : true;
-    this.balanceBedMeny = findIndex(this.rights, 'STAFF_BALANCE_SUPPLIES') === -1 ? false : true;
-    this.balanceSuppliesMenu = findIndex(this.rights, 'STAFF_BEDS') === -1 ? false : true;
-    this.statusTrackingMenu = findIndex(this.rights, 'STAFF_STATUS_TRACKING') === -1 ? false : true;
-    this.settingMenu = findIndex(this.rights, 'STAFF_SETTING') === -1 ? false : true;
+    this.rights = decoded.rights;
+    console.log(this.rights);
+
+    this.requisitionMenu = findIndex(this.rights, { name: 'STAFF_REQUISITION' }) === -1 ? false : true;
+    this.checkBedMenu = findIndex(this.rights, { name: 'STAFF_CHECK_BED' }) === -1 ? false : true;
+    this.checkSuppliesMenu = findIndex(this.rights, { name: 'STAFF_CHECK_SUPPLIES' }) === -1 ? false : true;
+    this.balanceBedMeny = findIndex(this.rights, { name: 'STAFF_BALANCE_SUPPLIES' }) === -1 ? false : true;
+    this.balanceSuppliesMenu = findIndex(this.rights, { name: 'STAFF_BEDS' }) === -1 ? false : true;
+    this.statusTrackingMenu = findIndex(this.rights, { name: 'STAFF_STATUS_TRACKING' }) === -1 ? false : true;
+    this.settingMenu = findIndex(this.rights, { name: 'STAFF_SETTING' }) === -1 ? false : true;
     this.hospTypeCode = decoded.hospTypeCode;
   }
 
