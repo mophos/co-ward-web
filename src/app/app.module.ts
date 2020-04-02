5d2e13212e27fb0cd029d2886e4ada67aea94ac5
peemppk <peemppk5538@gmail.com>
2020-04-02 18:50:38 +0700

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { NgSelect2Module } from 'ng-select2';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -42,7 +43,8 @@ export function tokenGetter() {
     ManagerModule,
     UserModule,
     FormsModule,
-    HelpModule
+    HelpModule,
+    NgSelect2Module
   ],
   providers: [
     { provide: 'API_URL', useValue: environment.apiUrl },
