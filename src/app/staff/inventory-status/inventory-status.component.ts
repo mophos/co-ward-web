dc3ba07cb3f3df78603b579ec1cdef6383159bcf
beer <beerde789@hotmail.com>
2020-04-16 11:28:30 +0700

    สถานะคงคลัง

--- src/app/staff/inventory-status/inventory-status.component.ts
+++ src/app/staff/inventory-status/inventory-status.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { InventoryService } from '../inventory.service';
+import { AlertService } from 'src/app/help/alert.service';
 
 @Component({
   selector: 'app-inventory-status',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class InventoryStatusComponent implements OnInit {
+  loading = false;
+  list: any;
+  total = 0;
+  limit = 20;
+  offset = 0;
 
-  constructor() { }
+  constructor(
+    private inventoryService: InventoryService,
+    private alertService: AlertService
+  ) { }
 
   ngOnInit() {
+    this.getInventoryStatus();
+  }
+
+  async getInventoryStatus() {
+    try {
+      this.loading = true;
+      const rs: any = await this.inventoryService.getInventoryStatus(this.limit, this.offset);
+      if (rs.ok) {
+        this.list = rs.rows;
+        this.total = rs.total;
+      } else {
+        this.alertService.error();
+      }
+      this.loading = false;
+    } catch (error) {
+      this.loading = false;
+      this.alertService.error(error);
+    }
   }
 
 }
