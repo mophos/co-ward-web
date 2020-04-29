6bcba1708ff56e2af0edb72f8974f3cd9c07e303
peemppk <peemppk5538@gmail.com>
2020-04-29 21:00:07 +0700

    report1

--- src/app/manager/report-dms/report-dms1/report-dms1.component.ts
+++ src/app/manager/report-dms/report-dms1/report-dms1.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ReportDmsService } from '../report-dms.service';
+import { AlertService } from '../../../help/alert.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-report-dms1',
@@ -7,9 +10,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportDms1Component implements OnInit {
 
-  constructor() { }
+  list: any;
+  @ViewChild('loading') loading: any;
+
+  constructor(
+    private reportService: ReportDmsService,
+    private alertService: AlertService
+  ) { }
 
   ngOnInit() {
+    this.getList();
+  }
+
+  async getList() {
+    this.loading.show();
+    try {
+      const rs: any = await this.reportService.getReport1();
+      if (rs.ok) {
+        this.list = rs.rows;
+        console.log(this.list);
+
+        this.loading.hide();
+      } else {
+        this.loading.hide();
+        this.alertService.error();
+      }
+    } catch (error) {
+      this.loading.hide();
+      this.alertService.error(error);
+    }
+  }
+
+  async doExportExcel() {
+    this.loading.show();
+    try {
+      const rs: any = await this.reportService.getReport1Excel();
+      console.log(rs);
+      if (!rs) {
+        this.loading.hide();
+      } else {
+        this.downloadFile('report-dms1', 'xlsx', rs);
+        // this.downloadFile('รายงานการจ่ายยา(แยกตามสถานที่จ่าย)', 'xlsx', url);
+        this.loading.hide();
+      }
+    } catch (error) {
+      console.log(error);
+      this.alertService.error();
+      this.loading.hide();
+    }
+  }
+
+  downloadFile(name, type, data: any) {
+    try {
+      const url = window.URL.createObjectURL(new Blob([data]));
+      console.log(url);
+      const fileName = `${name}.${type}`;
+      // Debe haber una manera mejor de hacer esto...
+      const a = document.createElement('a');
+      document.body.appendChild(a);
+      a.setAttribute('style', 'display: none');
+      a.href = url;
+      a.download = fileName;
+      a.click();
+      window.URL.revokeObjectURL(url);
+      a.remove(); // remove the element
+    } catch (error) {
+      console.log(error);
+      this.alertService.error();
+    }
   }
 
 }
