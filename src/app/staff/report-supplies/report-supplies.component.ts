eaa4136776e995acb11f6567e20b44210af5abad
gun khemnak <gunnn_boy_hand@hotmail.com>
2020-05-06 23:47:09 +0700

    push

--- src/app/staff/report-supplies/report-supplies.component.ts
+++ src/app/staff/report-supplies/report-supplies.component.ts
@@ -70,4 +70,44 @@ export class ReportSuppliesComponent implements OnInit {
     }
   }
 
+  async doExportExcel() {
+    this.loading.show();
+    try {
+      const date = this.date.date.year + '-' + this.date.date.month + '-' + this.date.date.day;
+      const rs: any = await this.service.getSupplieExport(date);
+      console.log(rs);
+      if (!rs) {
+        this.loading.hide();
+      } else {
+        this.downloadFile('report-supplies', 'xlsx', rs);
+        // this.downloadFile('รายงานการจ่ายยา(แยกตามสถานที่จ่าย)', 'xlsx', url);
+        this.loading.hide();
+      }
+    } catch (error) {
+      console.log(error);
+      this.alertService.error();
+      this.loading.hide();
+    }
+  }
+
+  downloadFile(name, type, data: any) {
+    try {
+      const url = window.URL.createObjectURL(new Blob([data]));
+      console.log(url);
+      const fileName = `${name}.${type}`;
+      // Debe haber una manera mejor de hacer esto...
+      const a = document.createElement('a');
+      document.body.appendChild(a);
+      a.setAttribute('style', 'display: none');
+      a.href = url;
+      a.download = fileName;
+      a.click();
+      window.URL.revokeObjectURL(url);
+      a.remove(); // remove the element
+    } catch (error) {
+      console.log(error);
+      this.alertService.error();
+    }
+  }
+
 }
