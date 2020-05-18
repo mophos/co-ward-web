4058737f267161380a814c2d6002e5fe2893f6f4
naphattharawat <naphattharawat@gmail.com>
2020-05-18 12:50:36 +0700

    report supplies

---
+++ src/app/staff/request-products/request-product-new/request-product-new.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-request-product-new',
+  templateUrl: './request-product-new.component.html',
+  styleUrls: ['./request-product-new.component.css']
+})
+export class RequestProductNewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
