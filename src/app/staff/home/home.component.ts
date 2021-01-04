7eb97e7f0bf97f17f73445b92d4b90a21a355d01
naphattharawat <naphattharawat@gmail.com>
2021-01-05 02:42:17 +0700

    alert bed

--- src/app/staff/home/home.component.ts
+++ src/app/staff/home/home.component.ts
@@ -1,3 +1,5 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { BedService } from './../bed.service';
 import { Component, OnInit } from '@angular/core';
 import { CovidCaseService } from './../services/covid-case.service';
 import { AlertService } from './../../help/alert.service';
@@ -8,17 +10,36 @@ import { AlertService } from './../../help/alert.service';
 })
 export class HomeComponent implements OnInit {
   list: any = [];
-
+  alertBed = false;
   // url: any = 'https://ops.moph.go.th/public/index.php/Portal_covid19_link_staff#';
   url: any = 'https://www.youtube.com/';
 
   constructor(
     private alertService: AlertService,
-    private covidCaseService: CovidCaseService
+    private covidCaseService: CovidCaseService,
+    private bedService: BedService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.getList();
+    this.getBed();
+  }
+  goToBed() {
+    this.router.navigate(['/staff/setting/beds']);
+  }
+
+  async getBed() {
+    try {
+      const rs: any = await this.bedService.getBeds();
+      if (rs.ok) {
+        if (!rs.rows.length) {
+          this.alertBed = true;
+        }
+      }
+    } catch (error) {
+      this.alertService.error(error);
+    }
   }
 
   async getList() {
