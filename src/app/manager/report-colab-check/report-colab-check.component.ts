ea4a0343ad48d6b8316c07a4f7f5cfc4108f51a2
naphattharawat <naphattharawat@gmail.com>
2021-04-26 19:33:50 +0700

    fix bug

--- src/app/manager/report-colab-check/report-colab-check.component.ts
+++ src/app/manager/report-colab-check/report-colab-check.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportColabCheckComponent implements OnInit {
 
+  loadings = false;
   list = [];
+  limit = 100;
+  totalList = 0;
   constructor() { }
 
   ngOnInit() {
