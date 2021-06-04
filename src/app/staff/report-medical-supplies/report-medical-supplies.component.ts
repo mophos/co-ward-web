2047a4eff6668eebc9e838bc75ef82d333cd95bc
peempol <peemppk5538@gmail.com>
2021-06-04 15:24:51 +0700

    pus

--- src/app/staff/report-medical-supplies/report-medical-supplies.component.ts
+++ src/app/staff/report-medical-supplies/report-medical-supplies.component.ts
@@ -1,6 +1,6 @@
 import { AlertService } from './../../help/alert.service';
 import { ReportService } from './../report.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-report-medical-supplies',
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportMedicalSuppliesComponent implements OnInit {
 
+  @ViewChild('loading', { static: true }) loading: any;
   list = [];
   constructor(
     private reportService: ReportService,
@@ -21,13 +22,17 @@ export class ReportMedicalSuppliesComponent implements OnInit {
 
   async getList() {
     try {
+      this.loading.show();
       const rs: any = await this.reportService.medicalSupplies();
       if (rs.ok) {
-          this.list = rs.rows;
+        this.list = rs.rows;
+        this.loading.hide();
       } else {
+        this.loading.hide();
         this.alertService.error(rs.error);
       }
     } catch (error) {
+      this.loading.hide();
       this.alertService.error(error);
     }
   }
